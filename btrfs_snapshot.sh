2428df5e4b0ef1a5107d97987fcc84c9b1a0a316
Henri Gasc <gasc@eurecom.fr>
2023-09-06 08:44:44 +0200

    Save all running users

--- btrfs_snapshot.sh
+++ btrfs_snapshot.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-NOW=$(date +"%Y-%m-%d_%H:%M:%S")
 
-snapshot_path="/home/gasc/.cache/snapshots"
-if [ ! -e "${snapshot_path}" ]; then
-  mkdir -p "${snapshot_path}"
-fi
+l_u="$(users)"
+
+for back_user in $(echo -e "${l_u// /\\n}" | sort -u); do
+  NOW=$(date +"%Y-%m-%d_%H:%M:%S")
+  snapshot_path="/home/${back_user}/.cache/snapshots"
+  if [ ! -e "${snapshot_path}" ]; then
+    mkdir -p "${snapshot_path}"
+  fi
  
-/sbin/btrfs subvolume snapshot /home/gasc "${snapshot_path}/backup_${NOW}"
+  /sbin/btrfs subvolume snapshot /home/${back_user} "${snapshot_path}/backup_${NOW}"
 
-# To make sure they are removed. (They should already be empty, but want to make sure)
-rm -rf "${snapshot_path}/backup_${NOW}/.cache/snapshots"
+  # To make sure they are removed. (They should already be empty, but want to make sure)
+  rm -rf "${snapshot_path}/backup_${NOW}/.cache/snapshots"
+done
