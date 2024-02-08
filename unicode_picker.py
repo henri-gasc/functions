933de7278febd98d05e7c5f4d022f20af9313f6e
Henri Gasc <gasc@eurecom.fr>
2024-02-08 13:51:25 +0100

    add unicode_picker

---
+++ unicode_picker.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from unicodedata import name
+
+for i in range(0x10FFFF):
+  try:
+    var = name(chr(i)).lower()
+    print(f"\\u{i:X}\t{chr(i)}\t{var}\tu{i:x}")
+  except:
+    var = None
