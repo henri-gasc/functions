a676c2c2c4f235dbf7af6a5419a2593d1c176e77
Henri Gasc <gasc@eurecom.fr>
2024-04-29 15:50:13 +0200

    Print name and update even when not 'update_git'

--- update_git.sh
+++ update_git.sh
@@ -9,11 +9,21 @@ pull_git_repo() {
     git submodule update --recursive
 }
 
+update_git_repo() {
+	echo "git pull $1"
+	tag="$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)"
+	pull_git_repo
+	new_tag="$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)"
+	if [ $? -eq 0 ] && [ "$new_tag" != "$tag" ]; then
+		echo "$d : $tag -> $new_tag" >> "${update_file}"
+	fi
+	echo ""
+}
+
 loop() {
     git rev-parse 2>/dev/null
     if [ $? -eq 0 ]; then
-        echo "git pull $1"
-        pull_git_repo
+		update_git_repo "$(basename ${PWD})"
     else
         for d in $(find . -mindepth 1 -maxdepth 1 -type d); do
             builtin cd "$d"
@@ -25,14 +35,7 @@ loop() {
 					echo "Going to ${branch_name}"
 					trash=`git checkout "${branch_name}"`
 				fi
-                echo "git pull $d"
-                tag="$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)"
-                pull_git_repo
-                new_tag="$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)"
-                if [ $? -eq 0 ] && [ "$new_tag" != "$tag" ]; then
-                    echo "$d : $tag -> $new_tag" >> "${update_file}"
-                fi
-                echo ""
+				update_git_repo "$d"
             else
                 loop
             fi
@@ -43,7 +46,6 @@ loop() {
 
 start_folder="$(pwd)"
 
-builtin cd "${GITDIR}"
 if [[ "$1" == "." ]]; then
 	builtin cd "${start_folder}"
 	loop "."
@@ -55,7 +57,9 @@ elif [[ "$@" != "" ]]; then
         builtin cd ".."
     done
 else
+	builtin cd "${GITDIR}"
     loop
+	builtin cd "${start_folder}"
 fi
 if [ ! -f "${update_file}" ]; then
     echo "No update were detected"
