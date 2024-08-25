70b509ab8805844ea0e33dc9166ece8ba7871a6a
Henri Gasc <gasc@eurecom.fr>
2024-08-25 22:44:28 +0200

    btrfs_snapshot: improvements
    In short:
    Now snapshot everything in /home and root
    Prepare the ground for customization of what to save, where to save it,
    and what to remove from the snapshot

--- btrfs_snapshot.sh
+++ btrfs_snapshot.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 
-l_u="$(users)"
+snapshot_user() {
+	NOW=$(date +"%Y-%m-%d_%H:%M:%S")
+
+	user_dir="$1"
+	path_config="${user_dir}/.config/snapshot"
+
+	DIR_TO_SAVE="${user_dir}"
+	DIR_SNAPSHOT="$2"
+	IGNORE_DIRS=""
+
+	# Customize things (read from config)
+
+	if [[ -f "${DIR_SNAPSHOT}/.no" ]]; then
+		echo "Not saving user ${back_user}"
+		return
+	fi
+
+	if [[ ! -d "${DIR_SNAPSHOT}" ]]; then
+		mkdir -p "${DIR_SNAPSHOT}"
+	fi
+
+	SNAPSHOT_PATH="${DIR_SNAPSHOT}/backup_${NOW}"
+
+	echo "Saving ${DIR_TO_SAVE} to ${SNAPSHOT_PATH}"
+	/usr/bin/btrfs subvolume snapshot "${DIR_TO_SAVE}" "${SNAPSHOT_PATH}"
+
+	# Remove directory to ignore
+	if [[ "${IGNORE_DIRS}" != "" ]]; then
+		IFS=' ' read -a array <<< "${IGNORE_DIRS}"
+		for d in "${array[@]}"; do
+			rm -rf "${SNAPSHOT_PATH}/$d"
+		done
+	fi
+
+	# To make sure we have no nested snapshots, they are removed.
+	# (It should already be empty, but I want to make sure).
+	rm -rf "${SNAPSHOT_PATH}/.cache/snapshots"
+}
+
+l_u="root $(ls /home)"
 
 for back_user in $(echo -e "${l_u// /\\n}" | sort -u); do
-  NOW=$(date +"%Y-%m-%d_%H:%M:%S")
-  snapshot_path="/home/${back_user}/.cache/snapshots"
-  if test -e "${snapshot_path}/.no"; then
-    echo "Not saving user ${back_user}"
-    continue
-  fi
-  if test ! -e "${snapshot_path}"; then
-    mkdir -p "${snapshot_path}"
-  fi
- 
-  /sbin/btrfs subvolume snapshot /home/${back_user} "${snapshot_path}/backup_${NOW}"
+	# Default path
+	user_path="/home/${back_user}"
+	if [[ "${back_user}" == "root" ]]; then
+		user_path="/root"
+	fi
+	snapshot_dir="${user_path}/.cache/snapshots"
 
-  # To make sure they are removed. (They should already be empty, but want to make sure)
-  rm -rf "${snapshot_path}/backup_${NOW}/.cache/snapshots"
+	snapshot_user "${user_path}" "${snapshot_dir}"
 done
