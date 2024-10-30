a51021f6dff3c1298532d1648f3e47c4640a0f39
Henri Gasc <gasc@eurecom.fr>
2024-10-30 10:44:34 +0100

    Allow steam to run for screen, and use swaylock for lock

--- toggle_lock.sh
+++ toggle_lock.sh
@@ -2,7 +2,7 @@
 
 line_lock="$(ps -u $USER -o pid,cmd | rg 'swayidle' | rg 'lock')"
 if [ "${line_lock}" == "" ]; then
-  swayidle timeout 300 '${HOME}/.local/bin/wayland_lock.sh' &
+  swayidle timeout 300 'swaylock' &
 else
   kill "$(echo ${line_lock} | cut -f 1 -d ' ')"
 fi
--- toggle_screen.sh
+++ toggle_screen.sh
@@ -2,7 +2,7 @@
 
 line_screen="$(ps -u $USER -o pid,cmd | rg 'swayidle' | rg 'dpms')"
 if [ "${line_screen}" == "" ]; then
-  swayidle -w timeout 60 'pidof vlc mpv mpvpaper steam ncmpcpp || hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' &
+  swayidle -w timeout 60 'pidof vlc mpv mpvpaper ncmpcpp || hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' &
 else
   kill "$(echo ${line_screen} | cut -f 1 -d ' ')"
 fi
