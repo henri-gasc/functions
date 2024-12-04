ca79dea083c451e02fdcdf5614c9f1069f9c560c
Henri Gasc <gasc@eurecom.fr>
2024-12-04 15:57:22 +0100

    toggle_lock: Use hyprlock

--- toggle_lock.sh
+++ toggle_lock.sh
@@ -2,7 +2,7 @@
 
 line_lock="$(ps -u $USER -o pid,cmd | rg 'swayidle' | rg 'lock')"
 if [ "${line_lock}" == "" ]; then
-  swayidle timeout 300 'swaylock' &
+  swayidle timeout 300 'hyprlock' &
 else
   kill "$(echo ${line_lock} | cut -f 1 -d ' ')"
 fi
