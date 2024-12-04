ea6c658c62f461f396326a49666e74a4ee7a7a17
Henri Gasc <gasc@eurecom.fr>
2024-12-04 15:58:54 +0100

    emerge_status: Restrict first message to 1 line
    In some situation (parallel emerge), gls would return many lines when we
    want a single line here.

--- emerge_status.sh
+++ emerge_status.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 arguments="--skip-file --fakeroots /mnt/foo /mnt/stable_stage3 / --show-root"
-echo "$(gls ${arguments} --read-ninja)"
+echo "$(gls ${arguments} --read-ninja | head -n 1)"
 echo "$(gls ${arguments} --all | tail -n 1)"
